0aacd9ad00eeb7c82cf4fe5ff3687de1acc87808
kl527 <kl527@cornell.edu>
2024-06-18 14:35:02 -0500

    6/18 2

--- app/melotomorrow/components/MTheader.tsx
+++ app/melotomorrow/components/MTheader.tsx
@@ -4,7 +4,7 @@ export const MTheader = (): JSX.Element => {
             <div className="mb-2 md:mb-5 text-melo-blue font-bold text-black text-lg md:text-[21px] tracking-[0] leading-[normal]">
                 MELO TOMORROW
             </div>
-            <p className="mb-7 md:mb-12 font-normal text-black text-[32px] md:text-[56px] tracking-[0] leading-[35px] md:leading-[55px]">
+            <p className="mb-5 md:mb-8 font-normal text-black text-[32px] md:text-[56px] tracking-[0] leading-[35px] md:leading-[55px]">
                 Clothing brand for <b>people with hypersensitivity</b></p>
             <div className="md:w-[646px] w-[237px] lg:w-[800px] lg:h-[7px] md:h-[5px] h-[4px] bg-black" />
         </div>
--- app/melotomorrow/page.tsx
+++ app/melotomorrow/page.tsx
@@ -1,12 +1,17 @@
 import { MTheader } from "./components/MTheader"
 import { MTleft } from "./components/MTleft"
+import Carousel from "./components/Carousel"
+
 export default function Page() {
     return (
         <main className="flex flex-row place-content-center mt-9 md:mt-[40px]">
             <div className="mr-8 md:mr-[80px] lg:mr-[150px] flex ">
                 <MTleft />
             </div>
+            <div>
             <MTheader />
+            <Carousel />
+            </div>
         </main>
     )
 }
