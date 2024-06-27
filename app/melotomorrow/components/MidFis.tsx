d88c63110261da357669272eb45035d776679185
kl527 <kl527@cornell.edu>
2024-06-27 14:28:53 -0500

    small mid fi fix

--- app/melotomorrow/components/MidFis.tsx
+++ app/melotomorrow/components/MidFis.tsx
@@ -13,7 +13,7 @@ export default function MidFis() {
         <div className="w-[237px] md:w-[646px] lg:w-[800px]">
             <h1 className="mb-1 text-black text-lg md:text-[21px] tracking-[0] leading-[normal]">Mid Fidelity Wireframes</h1>
             <p className="font-normal mb-1 text-black md:text-[20px] text-base tracking-[0] leading-7">
-While the team worked on finding a way to contact payment gateway services (due to Korea's strict regulations on payments), I focused on creating mid-fidelity wireframes with the low fidelity designs in mind.</p>
+                While the team worked on finding a way to contact payment gateway services (due to Korea&#x27;s strict regulations on payments), I focused on creating mid-fidelity wireframes with the low fidelity designs in mind.</p>
             <div>
                 <Image src="/midfis.jpg" className="mb-3 w-[237px] md:w-[646px] lg:w-[800px]" alt="Clothing Low Fidelity" width={800} height={400} />
             </div>
