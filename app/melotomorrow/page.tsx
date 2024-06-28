03a62c06204cea8f58e8f86499aff34b455d9e53
kl527 <kl527@cornell.edu>
2024-06-28 13:31:33 -0500

    preloader fix

--- app/melotomorrow/page.tsx
+++ app/melotomorrow/page.tsx
@@ -18,9 +18,9 @@ export default function Page() {
 
     return (
         <div>
-            {loading && <div className="flex 100vw place-content-center h-screen items-center">
+            {loading && <div className="flex 100vw place-content-center md:h-screen md:mt-0 mt-[150px] items-center">
                 <Preloader /> </div>}
-            {!loading && <main className={`w-[70%] ml-auto mr-auto pl-[15px] md:flex flex-row place-content-center mt-8 md:mt-[40px] ${!loading ? 'fadeIn' : ''}`}>
+            {!loading && <main className={`w-[70%] ml-auto mr-auto pl-[12px] md:flex flex-row place-content-center mt-8 md:mt-[40px] ${!loading ? 'fadeIn' : ''}`}>
                     <div className="mr-8 md:mr-[80px] lg:mr-[150px] flex ">
                         <MTleft />
                     </div>
