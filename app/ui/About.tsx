59624c65ff7bf3764bf7f769235df18eb3240268
kl527 <kl527@cornell.edu>
2024-07-23 14:51:39 -0500

    Books and Resume download added

--- app/ui/About.tsx
+++ app/ui/About.tsx
@@ -7,16 +7,21 @@ export default function About() {
             <div className="md:w-[646px] w-full lg:w-[800px]">
                 <p className="font-normal mb-3 text-black md:text-[20px] text-base tracking-[0] leading-[normal]">I&#x27;m also interested in a number of other things. Learn more about me in the links below!</p>
                 <div className='flex gap-3'>
-                <Link href="/melotomorrow" className="md:w-[135px] md:h-[40px] transition duration-500 group transition-colors duration-500 ease-in-out hover:bg-black flex w-[105px] h-9 items-center gap-2.5 pl-[9px] pr-[13px] py-[3px] left-0 border-4 border-solid border-black">
-                    <div className="text-center w-fit mr-[-7.00px] md:text-[19px] font-bold text-sm tracking-[0] leading-[normal] transition-colors duration-300 ease-in-out group-hover:text-white">
-                        About me →
-                    </div>
-                </Link>
-                <Link href="/melotomorrow" className="md:w-[95px] md:h-[40px] transition duration-500 group transition-colors duration-500 ease-in-out hover:bg-black flex w-[75px] h-9 bg-black items-center gap-2.5 pl-[9px] pr-[13px] py-[3px] left-0 border-4 border-solid border-black">
-                    <div className="text-center w-fit mr-[-7.00px] md:text-[19px] font-bold text-sm tracking-[0] leading-[normal] transition-colors duration-300 ease-in-out text-white group-hover:text-white">
-                        Resume
-                    </div>
-                </Link>
+                    <Link href="/melotomorrow" className="md:w-[135px] md:h-[40px] transition duration-500 group transition-colors duration-500 ease-in-out hover:bg-black flex w-[105px] h-9 items-center gap-2.5 pl-[9px] pr-[13px] py-[3px] left-0 border-4 border-solid border-black">
+                        <div className="text-center w-fit mr-[-7.00px] md:text-[19px] font-bold text-sm tracking-[0] leading-[normal] transition-colors duration-300 ease-in-out group-hover:text-white">
+                            About me →
+                        </div>
+                    </Link>
+                    <button className="md:w-[95px] md:h-[40px] transition duration-500 group transition-colors duration-500 ease-in-out hover:bg-black flex w-[75px] h-9 bg-black items-center gap-2.5 pl-[9px] pr-[13px] py-[3px] left-0 border-4 border-solid border-black">
+                        <a
+                            href='/resume/Kyung_Sub_Lee-CV.pdf'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className="text-center w-fit mr-[-7.00px] md:text-[19px] font-bold text-sm tracking-[0] leading-[normal] transition-colors duration-300 ease-in-out text-white group-hover:text-white"
+                        >
+                            Resume
+                        </a>
+                    </button>
                 </div>
             </div>
         </div>
--- app/ui/LeftBar.tsx
+++ app/ui/LeftBar.tsx
@@ -5,9 +5,6 @@ interface LeftBarProps {
 
 export const LeftBar: React.FC<LeftBarProps> = ({ isSidebarHidden, toggleSidebar }): JSX.Element => {
   return (
-    // <div className={`${isSidebarHidden ? 'w-12' : 'w-0'} fixed left-0 top-0 h-[100vh] 
-    // w-12 md:border-r-[15px] border-r-[12px] border-melo-blue
-    // `} />
     <div className={`sidebar ${isSidebarHidden ? 'sidebar--isHidden' : ''} md:border-r-[15px] border-r-[12px] border-melo-blue box-border`}>
       <div className="w-[85%] mr-auto ml-auto flex justify-end mt-[30px] md:w-[90%]">
         <button onClick={toggleSidebar} className="group md:border-[6px] mb-[20px] md:mb-[165px] transition-colors duration-300 ease-in-out hover:bg-black flex w-[45px] md:w-[60px] md:h-[86px] h-16 items-center justify-center border-4 border-solid border-black">
