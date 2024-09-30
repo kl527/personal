83245713c49d05788f747b19463bf60e1193359c
kl527 <kl527@cornell.edu>
2024-09-29 20:47:08 -0400

    CV update

--- app/about/components/experience.tsx
+++ app/about/components/experience.tsx
@@ -4,7 +4,7 @@ export default function Experience() {
       <div className="mb-2 md:mb-5 border-b-2 border-melo-blue w-fit font-bold text-black text-[30px] tracking-[0] leading-[normal]">
         EXPERIENCE
       </div>
-      <h1 className="text-[21px] leading-[20px]">UX Research Intern @ <em className="text-melo-blue">Finch</em></h1>
+      <h1 className="text-[21px] leading-[20px]">UI/UX Design Intern @ <em className="text-melo-blue">Finch</em></h1>
       <h2 className="font-normal text-[19px] mb-4">Aug. 2024 - Present</h2>
       <h1 className="text-[21px] leading-[20px]">Systems Engineer Intern @ <em className="text-melo-blue">InnovatEV Global</em></h1>
       <h2 className="font-normal text-[19px] mb-4">Jun. - Aug. 2024</h2>
