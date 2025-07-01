2110c720ae176c45bf939a591f97a89f3099d06f
kl527 <kl527@cornell.edu>
2025-07-01 15:14:04 -0400

    password protect

--- app/ui/Project6.tsx
+++ app/ui/Project6.tsx
@@ -11,7 +11,7 @@ export const Project6 = (): JSX.Element => {
         <div className="font-normal md:text-[20px] mt-3 md:mt-0 text-base tracking-[0] leading-[normal]">
           2025 | Research Paper in Progress
         </div>
-        <div className="font-bold mb-1 text-black text-lg md:text-[24px] tracking-[0] leading-[normal]">
+        <div className="font-bold mb-5 md:mb-1 text-black text-lg md:text-[24px] tracking-[0] leading-[normal]">
           Research in Progress
         </div>
         <p className="font-normal mb-2 text-black md:text-[20px] text-base tracking-[0] leading-[1.6] ">
